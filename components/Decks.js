afe0637ac82e62fe7927fe6be2d3935487c3b7b0
Stefan Adelbert <stefan.adelbert@gmail.com>
2018-04-09 14:37:58 +1000

    Added a redux store and Provider; Added Decks component; Added noop reducer; Added empty actions directory;

---
+++ components/Decks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class Decks extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Decks</Text>
+            </View>
+        );
+    }
+}
---
+++ reducers/index.js
@@ -0,0 +1,7 @@
+export default function entries (state = {}, action) {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+

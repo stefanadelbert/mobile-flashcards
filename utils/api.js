6a8c0fd948ab1fda287a3038553f90d058af6a91
Stefan Adelbert <stefan.adelbert@gmail.com>
2018-04-09 14:26:32 +1000

    Added api.js;

---
+++ utils/api.js
@@ -0,0 +1,17 @@
+import {AsynchStorage} from 'react-native';
+
+export function getDecks() {
+
+}
+
+export function getDeck(id) {
+    // take in a single id argument and return the deck associated with that id.
+}
+
+export function saveDeckTitle(title) {
+    // take in a single title argument and add it to the decks.
+}
+
+export function addCardToDeck(title, card) {
+    // take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+}
